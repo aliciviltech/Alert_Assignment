ca4dde3f85a836c33ca582b942f3f7f2cdfde7aa
aliciviltech <149861352+aliciviltech@users.noreply.github.com>
2024-02-28 19:55:58 +0500

    Two files are commited. App.js contains the alert codes.

---
+++ app.js
@@ -0,0 +1,15 @@
+// // Q1- 
+// alert('Assalam o Alaikum\nWelcome to Alert-Assignment');
+
+// // Q2- 
+// alert('Error! please enter a valid password.');
+
+// // Q3-
+// alert('Welcome to JS Land\nHappy Coding.');
+
+// // Q4-
+// alert('Welcome to JS Land...')
+// alert('Prevent this page from creating additional dialogue box.');
+
+// // Q5-
+// console.log("Hello I can run JS through my Eb Browser's console.")
